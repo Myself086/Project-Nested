5a0941acb6667bc9177fd8502358da6438cceb7b
Myself086 <laurentcourville@hotmail.com>
2021-07-22 16:56:28 -0400

    Add more opcodes to the interpreter

--- Assembly/Project/Interpreter.asm
+++ Assembly/Project/Interpreter.asm
@@ -3,6 +3,14 @@
 
 	// ---------------------------------------------------------------------------
 
+	.macro	Interpreter_ReadPC
+		lda	($.pc)
+	.endm
+
+	.macro	Interpreter_AdcReadPC
+		adc	($.pc)
+	.endm
+
 	.macro	Interpreter_ReadPCinc
 		lda	($.pc)
 		inc	$.pc
@@ -16,6 +24,76 @@ b_1__:
 		bne	$+b_1__
 			inc	$.pc+1
 b_1__:
+	.endm
+
+	.macro	Interpreter_IncPC16
+		inc	$.pc
+	.endm
+
+	.macro	Interpreter_DoubleIncPC16	reg
+		ld{0}	$.pc
+		in{0}
+		in{0}
+		st{0}	$.pc
+	.endm
+
+	.macro	Interpreter_Do		command
+		plp
+		{0}
+		php
+	.endm
+
+	.macro	Interpreter_Do2		command0, command1
+		plp
+		{0}
+		{1}
+		php
+	.endm
+
+	// ---------------------------------------------------------------------------
+
+	.macro	Interpreter_LoadAddr		reg, indexReg, do0, do1
+		.if	{1} == 0
+		{
+			rep	#0x30
+			Interpreter_ReadPC
+		}
+		.else
+		{
+			ldy	$.{1}
+			rep	#0x31
+			tya
+			Interpreter_AdcReadPC
+		}
+		Interpreter_DoubleIncPC16	y
+		call	Interpreter__ReadByte
+		//sep	#0x30
+		Interpreter_Do2	"{2}", "{3}"
+		.if	{0} != 0
+		{
+			sta	$.{0}
+		}
+		break
+	.endm
+
+	.macro	Interpreter_StoreAddr		reg, indexReg
+		ldx	$.{0}
+		.if	{1} == 0
+		{
+			rep	#0x30
+			Interpreter_ReadPC
+		}
+		.else
+		{
+			ldy	$.{1}
+			rep	#0x31
+			tya
+			Interpreter_AdcReadPC
+		}
+		Interpreter_DoubleIncPC16	y
+		call	Interpreter__WriteByte
+		sep	#0x30
+		break
 	.endm
 
 	// ---------------------------------------------------------------------------
@@ -77,13 +155,13 @@ Interpreter__Execute_Switch:
 		case	0x4c	// JMP addr -------------------------------------------------------
 			// Read destination
 			Interpreter_ReadPCinc
-			sta	$.addr
-			Interpreter_ReadPCinc
+			tax
+			Interpreter_ReadPC
 
 			// Is address pointing to ROM?
-			ora	#0
 			bpl	$+b_else
 				// Write address page
+				stx	$.addr
 				sta	$.addr+1
 				// Use indirect JMP
 				lda	$.a
@@ -103,10 +181,10 @@ Interpreter__Execute_Switch:
 				jmp	[$_JMPiU_Action]
 				.mx	0x30
 b_else:
+				Interpreter_IncPC
 				// Copy to PC
+				stx	$.pc
 				sta	$.pc+1
-				lda	$.addr
-				sta	$.pc
 				break
 
 		case	0x6c	// JMP (addr) -----------------------------------------------------
@@ -143,4 +221,189 @@ b_else:
 			lda	$_temp+1
 			jmp	[$_JMPiU_Action]
 			.mx	0x30
+		case	0xa5	// LDA dp ---------------------------------------------------------
+			Interpreter_ReadPCinc
+			tax
+			Interpreter_Do	"lda	$0x0000,x"
+			sta	$.a
+			break
+		case	0xa9	// LDA #const -----------------------------------------------------
+			Interpreter_Do	"Interpreter_ReadPC"
+			sta	$.a
+			Interpreter_IncPC
+			break
+		case	0xad	// LDA addr -------------------------------------------------------
+			Interpreter_LoadAddr		a, 0, "tax", ""
+			break
+		case	0xb9	// LDA addr,y -----------------------------------------------------
+			Interpreter_LoadAddr		a, y, "tax", ""
+			break
+		case	0xbd	// LDA addr,x -----------------------------------------------------
+			Interpreter_LoadAddr		a, x, "tax", ""
+			break
+		case	0xa2	// LDX #const -----------------------------------------------------
+			Interpreter_Do	"Interpreter_ReadPC"
+			sta	$.x
+			Interpreter_IncPC
+			break
+		case	0xa0	// LDY #const -----------------------------------------------------
+			Interpreter_Do	"Interpreter_ReadPC"
+			sta	$.y
+			Interpreter_IncPC
+			break
+		case	0x48	// PHA ------------------------------------------------------------
+			lda	$.a
+			Interpreter_Do	"pha"
+			break
+		case	0x08	// PHP ------------------------------------------------------------
+			Interpreter_Do	"php"
+			break
+		case	0x68	// PLA ------------------------------------------------------------
+			Interpreter_Do	"pla"
+			sta	$.a
+			break
+		case	0x28	// PLP ------------------------------------------------------------
+			pla
+			pla
+			and	#0xc3
+			ora	#0x30
+			pha
+			break
+		case	0x40	// RTI ------------------------------------------------------------
+			// Fix registers
+			lda	$.a
+			ldx	$.x
+			ldy	$.y
+			// Return
+			plp
+			plp
+			rtl
+		case	0x8d	// STA addr -------------------------------------------------------
+			Interpreter_StoreAddr		a, 0
+			break
+		case	0x99	// STA addr,y -----------------------------------------------------
+			Interpreter_StoreAddr		a, y
+			break
+		case	0x9d	// STA addr,x -----------------------------------------------------
+			Interpreter_StoreAddr		a, x
+			break
 
+	// ---------------------------------------------------------------------------
+
+	.mx	0x00
+	.func	Interpreter__ReadByte
+	// Entry: A = NES address, MX = Unknown
+Interpreter__ReadByte:
+	tay
+
+	// Which range are we in?
+	and	#0xe000
+	xba
+	tax
+	jmp	($_Interpreter__ReadByte_Switch,x)
+
+Interpreter__ReadByte_Switch_Trap:
+	trap
+
+Interpreter__ReadByte_Switch:
+	switch	0x80, Interpreter__ReadByte_Switch_Trap, Interpreter__ReadByte_Switch_Trap
+		case	0x00, 0x0f		// 0x00
+			lda	$0x0000,y
+			return
+
+		case	0x10, 0x1f		// 0x20
+		case	0x20, 0x2f		// 0x40
+			tyx
+			ldy	#_iIOPort_ldai*2
+			call	Recompiler__GetIOAccess
+			ldx	$.DP_ZeroBank-1
+			stx	$_InterpretIO_Action+1
+			sta	$_InterpretIO_Action
+			sep	#0x30
+			jsr	$=InterpretIO_Action_JMP
+			return
+
+			.macro	Interpreter__ReadByte_ChangeBank	bankRange
+				sep	#0x20
+				cmp	$_Memory_NesBank
+				beq	$+b_1__
+					sta	$_Memory_NesBank
+					lda	$_{0}+2
+					pha
+					plb
+b_1__:
+				lda	$0x0000,y
+				return
+			.endm
+		case	0x30, 0x3f		// 0x60
+			Interpreter__ReadByte_ChangeBank	Program_Bank_Sram
+		case	0x40, 0x4f		// 0x80
+			Interpreter__ReadByte_ChangeBank	Program_Bank_0
+		case	0x50, 0x5f		// 0xa0
+			Interpreter__ReadByte_ChangeBank	Program_Bank_1
+		case	0x60, 0x6f		// 0xc0
+			Interpreter__ReadByte_ChangeBank	Program_Bank_2
+		case	0x70, 0x7f		// 0xe0
+			Interpreter__ReadByte_ChangeBank	Program_Bank_3
+
+	// ---------------------------------------------------------------------------
+
+	.mx	0x00
+	.func	Interpreter__WriteByte
+	// Entry: A = NES address, X = Value, MX = Unknown
+Interpreter__WriteByte:
+	.local	_value
+	stx	$.value
+	tay
+
+	// Which range are we in?
+	and	#0xe000
+	xba
+	tax
+	jmp	($_Interpreter__WriteByte_Switch,x)
+
+Interpreter__WriteByte_Switch_Trap:
+	trap
+
+Interpreter__WriteByte_Switch:
+	switch	0x80, Interpreter__WriteByte_Switch_Trap, Interpreter__WriteByte_Switch_Trap
+		case	0x00, 0x0f		// 0x00
+			sep	#0x20
+			lda	$.value
+			sta	$0x0000,y
+			return
+
+		case	0x10, 0x1f		// 0x20
+		case	0x20, 0x2f		// 0x40
+		case	0x40, 0x4f		// 0x80
+		case	0x50, 0x5f		// 0xa0
+		case	0x60, 0x6f		// 0xc0
+		case	0x70, 0x7f		// 0xe0
+			tyx
+			ldy	#_iIOPort_stai*2
+			call	Recompiler__GetIOAccess
+			ldx	$.DP_ZeroBank-1
+			stx	$_InterpretIO_Action+1
+			sta	$_InterpretIO_Action
+			lda	$.value
+			sep	#0x30
+			jsr	$=InterpretIO_Action_JMP
+			return
+
+			.macro	Interpreter__WriteByte_ChangeBank	bankRange
+				sep	#0x20
+				cmp	$_Memory_NesBank
+				beq	$+b_1__
+					sta	$_Memory_NesBank
+					lda	$_{0}+2
+					pha
+					plb
+b_1__:
+				lda	$.value
+				sta	$0x0000,y
+				return
+			.endm
+		case	0x30, 0x3f		// 0x60
+			Interpreter__WriteByte_ChangeBank	Program_Bank_Sram
+
+	// ---------------------------------------------------------------------------
